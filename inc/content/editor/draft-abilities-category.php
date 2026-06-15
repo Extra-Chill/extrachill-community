a22a5fd197deb039768d19baa44e912c8db1ac93
Chris Huber <chubes@extrachill.com>
2026-06-15 21:50:38 +0000

    fix: guard ability category registration against double-fire _doing_it_wrong notice

--- inc/content/editor/draft-abilities-category.php
+++ inc/content/editor/draft-abilities-category.php
@@ -16,6 +16,14 @@ if ( did_action( 'wp_abilities_api_categories_init' ) ) {
 }
 
 function extrachill_community_register_ability_category() {
+	if ( ! function_exists( 'wp_register_ability_category' ) ) {
+		return;
+	}
+
+	if ( function_exists( 'wp_has_ability_category' ) && wp_has_ability_category( 'extrachill-community' ) ) {
+		return;
+	}
+
 	wp_register_ability_category(
 		'extrachill-community',
 		array(
